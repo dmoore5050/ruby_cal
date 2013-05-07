f2f8f3071a6782000f9bc4f3ae9ecc6404d53cfd
dmoore5050 <dustinmoore@gmail.com>
2013-05-07 07:52:09 -0500

    refactored ruby_cal initialize

--- ruby_cal.rb
+++ ruby_cal.rb
@@ -6,7 +6,7 @@ require_relative 'cal_year'
 class Cal
   attr_reader :month, :year
 
-  MONTH_ERROR = 'Valid months are 1..12, January..December'
+  MONTH_ERROR = 'Valid months are 1..12, January..December, jan..dec'
   YEAR_ERROR = 'Valid years are 1800..3000'
 
   MONTHS = %w(
@@ -30,23 +30,27 @@ class Cal
     @month = month_arg
     month_arg = month_arg.to_i if month_arg =~ /^[0-9]([0-9]*)?$/
 
-    if month_arg.class == String && month_arg.size >= 3
-      unless MONTHS.find { |month| /^#{ month_arg.downcase }/ =~ month.downcase }
-        raise NameError, MONTH_ERROR
-      end
+    case month_arg
+    when String
+      raise ArgumentError, MONTH_ERROR if month_arg.size < 3
+      raise_string_match_error month_arg
       find_matching_month month_arg
-    elsif month_arg.class == Fixnum
+    when Numeric
       raise ArgumentError, MONTH_ERROR unless (1..12).include? month_arg
       @month = month_arg
-    elsif month_arg.nil?
+    when NilClass
       @month = nil
-    else
-      raise ArgumentError, MONTH_ERROR
     end
 
     raise ArgumentError, YEAR_ERROR unless (1800..3000).include? year_arg.to_i
     @year = year_arg.to_i
 
+  end
+
+  def raise_string_match_error(month_arg)
+    unless MONTHS.find { |month| /^#{ month_arg.downcase }/ =~ month.downcase }
+      raise NameError, MONTH_ERROR
+    end
   end
 
   def find_matching_month(month_arg)
